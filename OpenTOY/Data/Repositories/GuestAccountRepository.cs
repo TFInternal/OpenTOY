ddf236a3301d063f7297270fd9bd89397da206ec
Nuutti <snuutti@protonmail.com>
2024-12-25 21:29:28 +0200

    Must have deleted this by accident at some point

--- OpenTOY/Data/Repositories/GuestAccountRepository.cs
+++ OpenTOY/Data/Repositories/GuestAccountRepository.cs
@@ -1,14 +1,23 @@
+using Microsoft.EntityFrameworkCore;
 using OpenTOY.Data.Entities;
 
 namespace OpenTOY.Data.Repositories;
 
 public interface IGuestAccountRepository : IRepository<GuestAccountEntity>
 {
+    Task<GuestAccountEntity?> GetByIdAsync(int serviceId, string deviceId);
 }
 
 public class GuestAccountRepository : RepositoryBase<GuestAccountEntity>, IGuestAccountRepository
 {
     public GuestAccountRepository(AppDb db) : base(db)
     {
+    }
+
+    public async Task<GuestAccountEntity?> GetByIdAsync(int serviceId, string deviceId)
+    {
+        return await Db.GuestAccounts
+            .Include(ga => ga.User)
+            .FirstOrDefaultAsync(ga => ga.ServiceId == serviceId && ga.DeviceId == deviceId);
     }
 }
